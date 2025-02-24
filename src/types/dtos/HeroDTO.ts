a1057b14eb83898acf92d4fc7cda7196111c4479
FabrizioCoder <fabrizioivan36@gmail.com>
2025-02-24 07:46:11 -0600

    update types

--- src/types/dtos/HeroDTO.ts
+++ src/types/dtos/HeroDTO.ts
@@ -4,16 +4,16 @@ export interface HeroDTO {
   readonly real_name: string;
   readonly imageUrl: string;
   readonly badges: Badges;
-  readonly ko: Ko;
-  readonly story_card: string;
-  readonly hero_card: string;
+  readonly ko?: Ko;
+  readonly story_card?: string;
+  readonly hero_card?: string;
   readonly role: string;
   readonly attack_type: string;
   readonly team: string[];
   readonly difficulty: string;
   readonly bio: string;
   readonly lore: string;
-  readonly lore_card: string;
+  readonly lore_card?: string;
   readonly transformations: Transformation[];
   readonly costumes: Costume[];
   readonly abilities: Ability[];
@@ -22,7 +22,7 @@ export interface HeroDTO {
 export interface Ability {
   readonly id: number;
   readonly icon: string;
-  readonly name?: string;
+  readonly name: string;
   readonly type: string;
   readonly isCollab: boolean;
   readonly description?: string;
@@ -31,46 +31,26 @@ export interface Ability {
 }
 
 export interface AdditionalFields {
-  readonly Key?: string;
-  readonly "Special Mechanic"?: string;
-  readonly Kick?: string;
-  readonly Punch?: string;
+  readonly Key: string;
+  readonly "Special Effect"?: string;
   readonly Damage?: string;
   readonly Casting?: string;
+  readonly "Attack Range"?: string;
   readonly "Attack Interval"?: string;
-  readonly "Maximum Distance"?: string;
-  readonly Charges?: string;
-  readonly "Critical Hit"?: string;
-  readonly "Damage Falloff"?: string;
-  readonly "Recharge Speed"?: string;
-  readonly "Projectile Speed"?: string;
-  readonly "Spider-Tracer Damage"?: string;
-  readonly "Spider-Tracer Duration"?: string;
   readonly Cooldown?: string;
-  readonly "Kick Damage"?: string;
-  readonly "Kicking Distance"?: string;
-  readonly "Projectile Damage"?: string;
-  readonly Range?: string;
   readonly Duration?: string;
+  readonly "Movement Boost"?: string;
+  readonly "Spell Field Range"?: string;
+  readonly "Maximum Distance"?: string;
+  readonly Range?: string;
   readonly "Energy Cost"?: string;
-  readonly "Slow Effect"?: string;
-  readonly "Slow Duration"?: string;
-  readonly "Stun Duration"?: string;
-  readonly "Special Effect"?: string;
-  readonly "Bonus Health (Self)"?: string;
-  readonly "Hits Required to Stun"?: string;
-  readonly "Disappearance Time To Trigger Detection"?: string;
-  readonly "Maximum Webbing Length"?: string;
-  readonly "Sprinting Speed"?: string;
-  readonly "Wall-Crawling Speed"?: string;
-  readonly Health?: string;
-  readonly "Movement Speed"?: string;
-  readonly "Team-Up Bonus"?: string;
+  readonly "Bonus Max Health"?: string;
+  readonly "Bonus Health Growth"?: string;
 }
 
 export interface Badges {
-  readonly gold: string;
-  readonly silver: string;
+  readonly gold?: string;
+  readonly silver?: string;
 }
 
 export interface Costume {
@@ -83,8 +63,8 @@ export interface Costume {
 }
 
 export interface Ko {
-  readonly prompt_1: string;
-  readonly prompt_2: string;
+  readonly prompt_1?: string;
+  readonly prompt_2?: string;
 }
 
 export interface Transformation {
--- src/types/dtos/PlayerDTO.ts
+++ src/types/dtos/PlayerDTO.ts
@@ -1,207 +1,184 @@
 export interface PlayerDTO {
-  uid: number;
-  name: string;
-  updates: Updates;
-  player: Player;
-  isPrivate: boolean;
-  overall_stats: OverallStats;
-  match_history: MatchHistory[];
-  rank_history: RankHistory[];
-  hero_matchups: HeroMatchup[];
-  team_mates: TeamMate[];
-  heroes_ranked: HeroesRanked[];
-  heroes_unranked: HeroesRanked[];
-  maps: Map[];
+  readonly uid: number;
+  readonly name: string;
+  readonly updates: Updates;
+  readonly player: Player;
+  readonly isPrivate: boolean;
+  readonly overall_stats: OverallStats;
+  readonly match_history: MatchHistory[];
+  readonly rank_history: any[];
+  readonly hero_matchups: HeroMatchup[];
+  readonly team_mates: TeamMate[];
+  readonly heroes_ranked: HeroesRanked[];
+  readonly heroes_unranked: HeroesRanked[];
+  readonly maps: Map[];
 }
 
 export interface HeroMatchup {
-  hero_id: number | null;
-  hero_name: string;
-  hero_class: HeroClass;
-  hero_thumbnail: null | string;
-  matches: number;
-  wins: number;
-  win_rate: string;
+  readonly hero_id: number;
+  readonly hero_name: string;
+  readonly hero_class: HeroClass;
+  readonly hero_thumbnail: string;
+  readonly matches: number;
+  readonly wins: number;
+  readonly win_rate: string;
 }
 
 export enum HeroClass {
   Duelist = "Duelist",
   Strategist = "Strategist",
-  Unknown = "Unknown",
   Vanguard = "Vanguard",
 }
 
 export interface HeroesRanked {
-  hero_id: number | null;
-  hero_name: string;
-  matches: number;
-  wins: number;
-  mvp: number;
-  svp: number;
-  kills: number;
-  deaths: number;
-  assists: number;
-  play_time: number;
-  damage: number;
-  heal: number;
-  damage_taken: number;
-  main_attack: MainAttack;
+  readonly hero_id: number;
+  readonly hero_name: string;
+  readonly hero_thumbnail: string;
+  readonly matches: number;
+  readonly wins: number;
+  readonly mvp: number;
+  readonly svp: number;
+  readonly kills: number;
+  readonly deaths: number;
+  readonly assists: number;
+  readonly play_time: number;
+  readonly damage: number;
+  readonly heal: number;
+  readonly damage_taken: number;
+  readonly main_attack: MainAttack;
 }
 
 export interface MainAttack {
-  total: number;
-  hits: number;
+  readonly total: number;
+  readonly hits: number;
 }
 
 export interface Map {
-  map_id: number;
-  map_thumbnail: string;
-  matches: number;
-  wins: number;
-  kills: number;
-  deaths: number;
-  assists: number;
-  play_time: number;
+  readonly map_id: number;
+  readonly map_thumbnail?: string;
+  readonly matches: number;
+  readonly wins: number;
+  readonly kills: number;
+  readonly deaths: number;
+  readonly assists: number;
+  readonly play_time: number;
 }
 
 export interface MatchHistory {
-  match_uid: string;
-  map_id: number;
-  map_thumbnail: string;
-  duration: number;
-  season: number;
-  winner_side: number;
-  mvp_uid: number;
-  svp_uid: number;
-  match_time_stamp: number;
-  play_mode_id: number;
-  game_mode_id: number;
-  score_info: ScoreInfo | null;
-  player_performance: PlayerPerformance;
+  readonly match_uid: string;
+  readonly map_id: number;
+  readonly map_thumbnail?: string;
+  readonly duration: number;
+  readonly season: number;
+  readonly winner_side: number;
+  readonly mvp_uid: number;
+  readonly svp_uid: number;
+  readonly match_time_stamp: number;
+  readonly play_mode_id: number;
+  readonly game_mode_id: number;
+  readonly score_info: { [key: string]: number } | null;
+  readonly player_performance: PlayerPerformance;
 }
 
 export interface PlayerPerformance {
-  player_uid: number;
-  hero_id: number | null;
-  kills: number;
-  deaths: number;
-  assists: number;
-  is_win: IsWin;
-  disconnected: boolean;
-  camp: number;
-  score_change: number | null;
-  level: number | null;
-  new_level: number | null;
-  new_score: number | null;
+  readonly player_uid: number;
+  readonly hero_id: number;
+  readonly kills: number;
+  readonly deaths: number;
+  readonly assists: number;
+  readonly is_win: IsWin;
+  readonly disconnected: boolean;
+  readonly camp: number;
+  readonly score_change: number | null;
+  readonly level: number | null;
+  readonly new_level: number | null;
+  readonly new_score: number | null;
 }
 
 export interface IsWin {
-  score: number;
-  is_win: boolean;
-}
-
-export interface ScoreInfo {
-  "0"?: number;
-  "1"?: number;
+  readonly score: number;
+  readonly is_win: boolean;
 }
 
 export interface OverallStats {
-  total_matches: number;
-  total_wins: number;
-  unranked: Ranked;
-  ranked: Ranked;
+  readonly total_matches: number;
+  readonly total_wins: number;
+  readonly unranked: Ranked;
+  readonly ranked: Ranked;
 }
 
 export interface Ranked {
-  total_matches: number;
-  total_wins: number;
-  total_assists: number;
-  total_deaths: number;
-  total_kills: number;
-  total_time_played: string;
-  total_time_played_raw: number
+  readonly total_matches: number;
+  readonly total_wins: number;
+  readonly total_assists: number;
+  readonly total_deaths: number;
+  readonly total_kills: number;
+  readonly total_time_played: string;
+  readonly total_time_played_raw: number;
 }
 
 export interface Player {
-  uid: number;
-  level: string;
-  name: string;
-  icon: Icon;
-  rank: Rank;
-  team: Team;
-  info: Info;
+  readonly uid: number;
+  readonly level: string;
+  readonly name: string;
+  readonly icon: Icon;
+  readonly rank: Rank;
+  readonly team: Team;
+  readonly info: Info;
 }
 
 export interface Icon {
-  player_icon_id: string;
-  player_icon: string;
-  banner?: string;
+  readonly player_icon_id: string;
+  readonly player_icon: string;
+  readonly banner?: string;
 }
 
 export interface Info {
-  completed_achievements: string;
-  login_os: string;
-  rank_game_season: RankGameSeason;
+  readonly completed_achievements: string;
+  readonly login_os: string;
+  readonly rank_game_season: { [key: string]: RankGameSeason };
 }
 
-export type RankGameSeason = Partial<Record<string, The100100>>
-
-export interface The100100 {
-  rank_game_id: number;
-  level: number;
-  rank_score: number;
-  max_level: number;
-  max_rank_score: number;
-  update_time: number;
-  win_count: number;
-  protect_score: number;
-  diff_score: number;
+export interface RankGameSeason {
+  readonly rank_game_id: number;
+  readonly level: number;
+  readonly rank_score: number;
+  readonly max_level: number;
+  readonly max_rank_score: number;
+  readonly update_time: number;
+  readonly win_count: number;
+  readonly protect_score: number;
+  readonly diff_score: number;
 }
 
 export interface Rank {
-  rank: string;
-  image: string | null;
-  color: string | null;
+  readonly rank: string;
+  readonly image: string;
+  readonly color: string;
 }
 
 export interface Team {
-  club_team_id: string;
-  club_team_mini_name: string;
-  club_team_type: string;
-}
-
-export interface RankHistory {
-  match_time_stamp: number;
-  level_progression: LevelProgression;
-  score_progression: ScoreProgression;
-}
-
-export interface LevelProgression {
-  from: number;
-  to: number;
-}
-
-export interface ScoreProgression {
-  add_score: number;
-  total_score: number;
+  readonly club_team_id: string;
+  readonly club_team_mini_name: string;
+  readonly club_team_type: string;
 }
 
 export interface TeamMate {
-  player_info: PlayerInfo;
-  matches: number;
-  wins: number;
-  win_rate: string;
+  readonly player_info: PlayerInfo;
+  readonly matches: number;
+  readonly wins: number;
+  readonly win_rate: string;
 }
 
 export interface PlayerInfo {
-  nick_name: string;
-  player_icon: string;
-  player_uid: number;
+  readonly nick_name: string;
+  readonly player_icon: string;
+  readonly player_uid: number;
 }
 
 export interface Updates {
-  info_update_time: string;
-  last_history_update: string | null;
-  last_inserted_match: string | null;
-  last_update_request: string | null;
+  readonly info_update_time: string;
+  readonly last_history_update: string;
+  readonly last_inserted_match: string;
+  readonly last_update_request: string;
 }

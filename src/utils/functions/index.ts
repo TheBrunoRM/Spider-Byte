c622f5d7cf0c19489633421b888b423fea631331
FabrizioCoder <fabrizioivan36@gmail.com>
2025-02-26 00:45:27 -0600

    removed unused functions

--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -1,11 +1,9 @@
 import { scrapePatchNotes } from './scrapePatchNotes';
 import { capitalize } from './capitalize';
-import { cleanText } from './cleanText';
 import { getEmoji } from './getEmoji';
 
 export default {
     scrapePatchNotes,
     capitalize,
-    cleanText,
     getEmoji
 };
